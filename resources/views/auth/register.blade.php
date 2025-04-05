3b95aeaa3c3c518e96f01a9a951a1cbfe6945e9f
Попехин Константин Сергеевич <popekhin80@bk.ru>
2025-04-05 22:48:01 +0700

    Обновлён фронт для /register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,26 @@
     <style>
         body {
             font-family: "Arial", sans-serif;
+            margin:0;
+            padding:0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        .box {
+            border: solid 1px gray;
+            display: inline-block;
+            padding: 15px;
+            line-height: normal;
+            vertical-align: center;
+        }
+        .input_box{
+            margin: 10px;
+        }
+        .input_box input {
+            padding: 5px;
+            width: 93%;
         }
         a {
             text-decoration: none;
@@ -15,35 +35,51 @@
             border: solid 1px black;
             transition: ease .3s;
         }
-
         a:hover {
             color: black;
             background: white;
         }
-        form {
-            margin: 40px;
-        }
-        form input[type="submit"]{
+
+        .box input[type="submit"]{
             background: cornflowerblue;
+            color: white;
+            border: solid 1px black;
+            border-radius: 4px;
+            transition: ease .3s;
+            padding: 20px;
+            font-size: 16px;
+        }
+        .box input[type="submit"]:hover{
+            cursor: pointer;
+            color: black;
+            background: white;
         }
     </style>
 </head>
 <body>
-    <a href="{{ url()->previous() }}">Back</a>
+<div class="box">
     <form action="/register" method="post">
         @csrf
-        <label for="login">Login: </label>
-        <input type="text" name="login">
-        <label for="email">Email: </label>
-        <input type="text" name="email">
-        <label for="password">Password: </label>
-        <input type="password" name="password">
+        <div class="input_box">
+            <label for="login">Login: </label>
+            <input type="text" name="login"><br>
+        </div>
+        <div class="input_box">
+            <label for="email">Email: </label>
+            <input type="text" name="email"><br>
+        </div>
+        <div class="input_box">
+            <label for="password">Password: </label>
+            <input type="password" name="password"><br>
+        </div>
         <input type="submit" value="Register">
+        <a href="{{ url()->previous() }}" style="float: right">Back</a>
     </form>
     <ul>
         @foreach($errors->all() as $error)
             <li>{{$error}}</li>
         @endforeach
     </ul>
+</div>
 </body>
 </html>
--- storage/framework/views/566fc3ddf28bb6bf22fac969cd05d92d.php
+++ storage/framework/views/566fc3ddf28bb6bf22fac969cd05d92d.php
@@ -5,6 +5,26 @@
     <style>
         body {
             font-family: "Arial", sans-serif;
+            margin:0;
+            padding:0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        .box {
+            border: solid 1px gray;
+            display: inline-block;
+            padding: 15px;
+            line-height: normal;
+            vertical-align: center;
+        }
+        .input_box{
+            margin: 10px;
+        }
+        .input_box input {
+            padding: 5px;
+            width: 93%;
         }
         a {
             text-decoration: none;
@@ -15,36 +35,52 @@
             border: solid 1px black;
             transition: ease .3s;
         }
-
         a:hover {
             color: black;
             background: white;
         }
-        form {
-            margin: 40px;
-        }
-        form input[type="submit"]{
+
+        .box input[type="submit"]{
             background: cornflowerblue;
+            color: white;
+            border: solid 1px black;
+            border-radius: 4px;
+            transition: ease .3s;
+            padding: 20px;
+            font-size: 16px;
+        }
+        .box input[type="submit"]:hover{
+            cursor: pointer;
+            color: black;
+            background: white;
         }
     </style>
 </head>
 <body>
-    <a href="<?php echo e(url()->previous()); ?>">Back</a>
+<div class="box">
     <form action="/register" method="post">
         <?php echo csrf_field(); ?>
-        <label for="login">Login: </label>
-        <input type="text" name="login">
-        <label for="email">Email: </label>
-        <input type="text" name="email">
-        <label for="password">Password: </label>
-        <input type="password" name="password">
+        <div class="input_box">
+            <label for="login">Login: </label>
+            <input type="text" name="login"><br>
+        </div>
+        <div class="input_box">
+            <label for="email">Email: </label>
+            <input type="text" name="email"><br>
+        </div>
+        <div class="input_box">
+            <label for="password">Password: </label>
+            <input type="password" name="password"><br>
+        </div>
         <input type="submit" value="Register">
+        <a href="<?php echo e(url()->previous()); ?>" style="float: right">Back</a>
     </form>
     <ul>
         <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <li><?php echo e($error); ?></li>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     </ul>
+</div>
 </body>
 </html>
 <?php /**PATH C:\Users\Konstantin\technicalsupport\resources\views/auth/register.blade.php ENDPATH**/ ?>
